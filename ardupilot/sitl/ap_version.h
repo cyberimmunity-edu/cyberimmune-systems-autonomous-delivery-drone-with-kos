dbf2fa458ec355ae4fd43a6cc0b910279f2b1aae
Andrey Chernov <a.o.chernov@mail.ru>
2024-04-26 19:35:08 +0300

    fix: servo pwm -- 1200 for open and 1800 for close

--- ardupilot/sitl/ap_version.h
+++ ardupilot/sitl/ap_version.h
@@ -6,5 +6,5 @@
 #error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
 #endif
 
-#define GIT_VERSION "58d5c09b"
-#define GIT_VERSION_INT 1490403483
+#define GIT_VERSION "83462520"
+#define GIT_VERSION_INT 2202412320
