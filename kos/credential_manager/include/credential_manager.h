d54071056049e36d61344dcd96d249a160add27a
Andrey Chernov <a.o.chernov@mail.ru>
2024-05-27 14:20:13 +0300

    feat: drone RSA key is stored on SD-card

--- kos/credential_manager/include/credential_manager.h
+++ kos/credential_manager/include/credential_manager.h
@@ -3,8 +3,15 @@
 #include <stdint.h>
 
 int generateRsaKey();
+int loadRsaKey(uint8_t* N, uint8_t* D, char* n, char* e, uint32_t nLen, uint32_t eLen);
 int shareRsaKey();
+
+int getRsaKey();
 int setRsaKey(char* key);
 
 int signMessage(char* message, char* sign);
 int checkSignature(char* message, uint8_t &correct);
+
+char* getKeyN();
+char* getKeyE();
+char* getKeyD();
