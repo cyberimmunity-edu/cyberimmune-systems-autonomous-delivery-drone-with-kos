acfa27d6c110989961a7bfc4b8ae36fa78d5c748
Andrey Chernov <a.o.chernov@mail.ru>
2024-07-10 05:14:57 +0300

    fix: sitl with obstacles is updated

--- ardupilot/sitl_obstacles/libraries/GCS_MAVLink/include/mavlink/v2.0/all/all.h
+++ ardupilot/sitl_obstacles/libraries/GCS_MAVLink/include/mavlink/v2.0/all/all.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_ALL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_ALL_XML_HASH 6949390581371468692
+#define MAVLINK_ALL_XML_HASH 5022500357473636147
 
 #ifdef __cplusplus
 extern "C" {
--- ardupilot/sitl_obstacles/libraries/GCS_MAVLink/include/mavlink/v2.0/minimal/minimal.h
+++ ardupilot/sitl_obstacles/libraries/GCS_MAVLink/include/mavlink/v2.0/minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH -28202364297463352
+#define MAVLINK_MINIMAL_XML_HASH -8750058940536915703
 
 #ifdef __cplusplus
 extern "C" {
