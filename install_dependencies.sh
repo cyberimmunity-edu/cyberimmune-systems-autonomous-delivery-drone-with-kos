a7a5eca5564f4f6c02b32a9a19c7735817411bec
Andrey Chernov <a.o.chernov@mail.ru>
2024-09-09 11:46:05 +0300

    feat: Split terminal for run.sh script is done

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/bash
 sudo apt update
 sudo apt upgrade
+sudo apt install tmux
+sudo apt install unzip
 sudo apt install ./KasperskyOS-Community-Edition-1.2.0.89_en.deb
 sudo rm -f /opt/KasperskyOS-Community-Edition-1.2.0.89/sysroot-aarch64-kos/bin/dnet_entity
 sudo unzip -j KasperskyOS-Community-Edition-1.2.0.45.zip KasperskyOS-Community-Edition-1.2.0.45/sysroot-aarch64-kos/bin/dnet_entity -d /opt/KasperskyOS-Community-Edition-1.2.0.89/sysroot-aarch64-kos/bin
@@ -11,3 +13,4 @@ sudo pip3 install mavproxy
 sudo add-apt-repository universe
 sudo apt install libfuse2
 pip install --target mavproxy/ mavproxy
+sudo chmod -R 777 mavproxy
