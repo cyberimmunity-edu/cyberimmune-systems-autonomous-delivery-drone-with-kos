85b004648956f88284889ff23e03162868665863
Alexander Serokhvostov <dope.deala123@gmail.com>
2025-02-03 19:50:01 +0300

    remove altitude from marker description, add marker rotation

--- orvd/static/admin.js
+++ orvd/static/admin.js
@@ -473,17 +473,37 @@ async function get_mission(id) {
 let vehicles = {};
 
 function add_or_update_vehicle_marker(id, lat, lon, alt, azimuth, speed) {
+  let rotationInRadians = (90 - azimuth) * Math.PI / 180;
+  let vehicleStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+      anchor: [0.5, 0.5],
+      src: 'static/resources/vehicle_marker.svg',
+      scale: 0.25,
+      rotation: rotationInRadians
+    })
+  });
+  
+  let inactiveVehicleStyle = new ol.style.Style({
+      image: new ol.style.Icon({
+        anchor: [0.5, 0.5],
+        src: 'static/resources/vehicle_marker.svg',
+        scale: 0.25,
+        opacity: 0.6,
+        rotation: rotationInRadians
+      })
+    });
+
   if (!vehicles[id]) {
     let vehicle = new ol.Feature(new ol.geom.Point([lon, lat]));
     vehicle.setId(`uav${id}`);
-    vehicle.setStyle(id === active_id ? uav_style : inactive_uav_style);
-    vehicle.set('description', `ID: ${id}\nВысота: ${alt}\nСкорость: ${speed}\nНаправление: ${azimuth}`);
+    vehicle.setStyle(id === active_id ? vehicleStyle : inactiveVehicleStyle);
+    vehicle.set('description', `ID: ${id}\nСкорость: ${speed}\nНаправление: ${azimuth}`);
     vehicles[id] = vehicle;
     markers.getSource().addFeature(vehicles[id]);
   } else {
     vehicles[id].getGeometry().setCoordinates([lon, lat]);
-    vehicles[id].set('description', `ID: ${id}\nВысота: ${alt}\nСкорость: ${speed}\nНаправление: ${azimuth}`);
-    vehicles[id].setStyle(id === active_id ? uav_style : inactive_uav_style);
+    vehicles[id].set('description', `ID: ${id}\nСкорость: ${speed}\nНаправление: ${azimuth}`);
+    vehicles[id].setStyle(id === active_id ? vehicleStyle : inactiveVehicleStyle);
   }
 }
 
