019dfb867b352df548a507c54dc362085d6790ba
Sergey Sobolev <s.sobolev@spbu.ru>
2025-03-18 17:21:34 +0300

    chore: use apt-get instead of apt

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
-sudo add-apt-repository universe -y && sudo apt update
-sudo apt upgrade
-sudo apt install tmux unzip python3 python3-pip libfuse2 ./KasperskyOS-Community-Edition-RaspberryPi4b-1.3.0.166_ru.deb
+sudo add-apt-repository universe -y && sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install tmux unzip python3 python3-pip libfuse2 ./KasperskyOS-Community-Edition-RaspberryPi4b-1.3.0.166_ru.deb
 sudo pip3 install pyserial mavproxy
 pip install --target mavproxy/ mavproxy
 sudo chmod -R 777 mavproxy
